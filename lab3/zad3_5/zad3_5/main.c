f21ea69d32a413e3ebb205c4facba6fc97ec0168
Piotr Dominiak <piotrdominiak@MacBook-Air-Piotr.local>
2017-11-05 10:01:04 +0100

    lab31

--- lab3/zad3_5/zad3_5/main.c
+++ lab3/zad3_5/zad3_5/main.c
@@ -1,8 +1,32 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#define WYMIAR 10
+
+int zmienna_wspolna = 0;
+
+void *zadanie_watku (void * arg_wsk)
+{
+    
+    int id;
+    id = *((int*)arg_wsk);
+    printf("\t ID watku : %d.ID systemowe watku : %d\n",id, pthread_self());
+           
+   return(NULL);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    pthread_t watek[WYMIAR];
+ //   void *wynik;
+    int i;
+    for (i = 0;i<WYMIAR;i++)
+        pthread_create(&watek[i],NULL,zadanie_watku,&i);
+    for (i = 0;i<WYMIAR;i++)
+        pthread_join(watek[i], NULL);
+    
+    pthread_exit(NULL);
+    
     return 0;
 }
+
+
